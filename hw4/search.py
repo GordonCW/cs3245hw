1ee044e65d50e546f8724a7c5b3b7f47ff633149
Chung Wa KO <gordon.cw.ko@gmail.com>
2018-04-25 16:55:53 +0800

    modified the threshold of result of free text query

--- hw4/search.py
+++ hw4/search.py
@@ -191,7 +191,8 @@ def cosineScore(q, booleanQuery):
     if booleanQuery == True:
         nonzeroScoresList = [(x[0], x[1]) for x in scores if x[1] != None]
     else:
-        nonzeroScoresList = [(x[0], x[1]) for x in scores if x[1] != None and x[2] > (ORIGINAL_NO-1)]
+        nonzeroScoresList = [(x[0], x[1]) for x in scores if x[1] != None and x[2] >= math.ceil(ORIGINAL_NO/4)]
+        print("Threshold:", math.ceil(ORIGINAL_NO/4))
     nonzeroScoresList.sort(key=lambda x: x[1], reverse=True)
     result = nonzeroScoresList
 
@@ -495,30 +496,30 @@ with open(file_of_output, "w", encoding="utf-8") as t:
 
 
 
-        # # print(queryResult)
-        # if queryResult != None:
-        #     print("number of result: ", len(queryResult))
+        # print(queryResult)
+        if queryResult != None:
+            print("number of result: ", len(queryResult))
 
-        #     query_no = int(file_of_output.split('.')[1])
+            query_no = int(file_of_output.split('.')[1])
 
-        #     # evaluate score
-        #     required = []
-        #     # q1
-        #     required.append([6807771, 4001247, 3992148])
+            # evaluate score
+            required = []
+            # q1
+            required.append([6807771, 4001247, 3992148])
 
-        #     # q2
-        #     required.append([2211154, 2748529])
+            # q2
+            required.append([2211154, 2748529])
 
-        #     # q3
-        #     required.append([4273155, 3243674, 2702938])
+            # q3
+            required.append([4273155, 3243674, 2702938])
 
-        #     requiredDic = {}
+            requiredDic = {}
 
-        #     for i in range(len(queryResult)):
-        #         if queryResult[i] in required[query_no-1]:
-        #             requiredDic[queryResult[i]] = i
+            for i in range(len(queryResult)):
+                if queryResult[i] in required[query_no-1]:
+                    requiredDic[queryResult[i]] = i
 
-        #     print(requiredDic)
+            print(requiredDic)
 
         if queryResult == None:
             t.write('\n')
